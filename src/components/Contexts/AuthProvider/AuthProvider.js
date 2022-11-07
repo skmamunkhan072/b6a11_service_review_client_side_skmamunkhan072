db09177d49ef0a1897e7236490a28b432683eac6
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-11-08 02:31:00 +0600

    Folder stackcer addd

---
+++ src/components/Contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthProvider = ({ children }) => {
+  return <div></div>;
+};
+
+export default AuthProvider;
---
+++ src/components/Pages/SingIn/SingIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingIn = () => {
+  return (
+    <div>
+      <h1>This is Sing in comppo</h1>
+    </div>
+  );
+};
+
+export default SingIn;
